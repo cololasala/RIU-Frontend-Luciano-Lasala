5a50581329effbb473c0a5e841e97c6408694aa1
cololasala <luciano.lasala12@hotmail.com>
2026-08-05 09:30:40 -0300

    add testing to directive

--- src/app/directives/hero-name-to-upper-case.directive.spec.ts
+++ src/app/directives/hero-name-to-upper-case.directive.spec.ts
@@ -1,8 +1,18 @@
 import { HeroNameToUpperCase } from './hero-name-to-upper-case.directive';
 
+let directive: HeroNameToUpperCase;
+
 describe('HeroNameToUpperCase', () => {
+  directive = new HeroNameToUpperCase();
+
   it('should create an instance', () => {
-    const directive = new HeroNameToUpperCase();
     expect(directive).toBeTruthy();
+  });
+
+  it('should transform the input value to uppercase, only first letter', () => {
+    const inputElement = document.createElement('input');
+    inputElement.value = 'superman';
+    directive.transformToUpperCase({ target: inputElement } as unknown as Event);
+    expect(inputElement.value).toBe('Superman');
   });
 });
